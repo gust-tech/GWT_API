5a92970055ad94b358c2ed377cd165fc18ae32f7
Lady Debug <117478012+Equipe03-Generation@users.noreply.github.com>
2023-01-10 10:15:20 -0300

    Versão 1.4.1

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,35 +11,35 @@ import { TurmaModule } from './turma/turma.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_genworktable',
-      entities: [Projeto, Turma, Grupo],
-      synchronize: true
-    }),
-
     // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   url: process.env.DATABASE_URL,
-    //   logging: false,
-    //   dropSchema: false,
-    //   ssl: {
-    //     rejectUnauthorized: false,
-    //   },
-    //   synchronize: true,
-    //   autoLoadEntities: true,
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'db_genworktable',
+    //   entities: [Projeto, Turma, Grupo],
+    //   synchronize: true
     // }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      logging: false,
+      dropSchema: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
     
     ProjetoModule,
     TurmaModule,
     GrupoModule
   ],
 
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule { }
